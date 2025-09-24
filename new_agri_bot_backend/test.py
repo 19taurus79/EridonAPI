3a4d1744d979fc06058a31e0825bbe16f29543ec
Onyshchenko Serhii <123294835+19taurus79@users.noreply.github.com>
2025-09-24 17:15:28 +0300

    create tasks

--- new_agri_bot_backend/test.py
+++ new_agri_bot_backend/test.py
@@ -1,26 +1,31 @@
+import json
 import os.path
+from datetime import date, timedelta, datetime, timezone
 from pprint import pprint
 
+from aiogram import Bot
 from google.auth.transport.requests import Request
 from google.oauth2.credentials import Credentials
 from google_auth_oauthlib.flow import InstalledAppFlow
 from googleapiclient.discovery import build
 from googleapiclient.errors import HttpError
-
+from .config import TELEGRAM_BOT_TOKEN
 from new_agri_bot_backend.tables import Tasks
 
 # If modifying these scopes, delete the file token.json.
 SCOPES = ["https://www.googleapis.com/auth/tasks"]
+
+bot = Bot(TELEGRAM_BOT_TOKEN)
 
 
 async def get_all_tasks(user):
     """Shows basic usage of the Tasks API.
     Prints the title and ID of the first 10 task lists.
     """
-    creds = None
-    # The file token.json stores the user's access and refresh tokens, and is
-    # created automatically when the authorization flow completes for the first
-    # time.
+    # creds = None
+    # # The file token.json stores the user's access and refresh tokens, and is
+    # # created automatically when the authorization flow completes for the first
+    # # time.
     BASE_DIR = os.path.dirname(os.path.abspath(__file__))
     TOKEN_ACCOUNT_FILE = os.path.join(BASE_DIR, "token.json")
     SERVICE_ACCOUNT_FILE = os.path.join(BASE_DIR, "credentials_task.json")
@@ -38,7 +43,7 @@ async def get_all_tasks(user):
         # Save the credentials for the next run
         with open(TOKEN_ACCOUNT_FILE, "w") as token:
             token.write(creds.to_json())
-
+    #
     try:
         service = build("tasks", "v1", credentials=creds)
 
@@ -54,15 +59,42 @@ async def get_all_tasks(user):
             .execute()
         )
         items = results.get("items", [])
-        if user.is_admin:
-            return items
-        user_tasks = (
-            await Tasks.select().where(Tasks.task_creator == user.telegram_id).run()
+
+        def date_minus_3_days():
+            """Возвращает datetime-объект за 3 дня до текущего момента в UTC."""
+            # Получаем текущее время в UTC и вычитаем 3 дня
+            # Использование timezone.utc - лучший и более современный способ
+            return datetime.now(timezone.utc) - timedelta(days=3)
+
+        filter_date = date_minus_3_days()
+        all_tasks = (
+            await Tasks.select()
+            .where(
+                (Tasks.task_status != 2)
+                | ((Tasks.created_at > filter_date) & (Tasks.task_status == 2))
+            )
+            .order_by(Tasks.task_status)
+            .order_by(Tasks.created_at, ascending=False)
+            .run()
         )
-        user_tasks_value = []
-        for task in user_tasks:
-            user_tasks_value.append(task["task_id"])
-        filtered_tasks = [item for item in items if item["id"] in user_tasks_value]
+        if user.is_admin:
+            return all_tasks
+        else:
+            user_tasks = (
+                await Tasks.select().where((Tasks.task_creator == user.telegram_id))
+                & (
+                    (Tasks.task_status != 2)
+                    | ((Tasks.created_at > filter_date) | (Tasks.task_status == 2))
+                )
+                .order_by(Tasks.task_status)
+                .order_by(Tasks.created_at, ascending=False)
+                .run()
+            )
+        return user_tasks
+        # user_tasks_value = []
+        # for task in user_tasks:
+        #     user_tasks_value.append(task["task_id"])
+        # filtered_tasks = [item for item in items if item["id"] in user_tasks_value]
         # if not items:
         #     print("No task lists found.")
         #     return
@@ -95,7 +127,7 @@ async def get_all_tasks(user):
         #         print(
         #             f"- {task['title']} (Статус: {task.get('status', 'no status')}, ID:{task['id']})"
         #         )
-        return filtered_tasks
+        # return filtered_tasks
     except HttpError as err:
         print(err)
 
@@ -273,8 +305,21 @@ async def create_task(date, note, title, user):
             .execute()
         )
         await Tasks.insert(
-            Tasks(task_id=results["id"], task_creator=user.telegram_id, task_status=0)
+            Tasks(
+                task_id=results["id"],
+                task_creator=user.telegram_id,
+                task_creator_name=user.full_name_for_orders,
+                task_status=0,
+                task=title,
+            )
         )
+        admins_json = os.getenv("ADMINS", "[]")
+        admins = json.loads(admins_json)
+        for admin in admins:
+            await bot.send_message(
+                chat_id=admin, text="З'явилось нове завдання", parse_mode="HTML"
+            )
+
         print(user)
         print(results["webViewLink"])
     except HttpError as err:
